55213965c543675ee933941733f937e1d77683e5
Syed Husnain Haider Bukhari <65342860+revolutionarybukhari@users.noreply.github.com>
2023-09-08 01:39:19 +0500

    Update dependency.py

--- dependency.py
+++ dependency.py
@@ -7,45 +7,24 @@ from pathlib import Path
 from datetime import datetime
 
 def install_packages_but_jank_af():
-    packages = ['build-essential', 'python3-dev', 'ffmpeg', 'aria2']
+    # You can skip the installation of system packages in Kaggle
     pip_packages = ['pip', 'setuptools', 'wheel', 'httpx==0.23.0', 'faiss-gpu', 'fairseq', 'gradio==3.34.0',
                     'ffmpeg', 'ffmpeg-python', 'praat-parselmouth', 'pyworld', 'numpy==1.23.5',
                     'numba==0.56.4', 'librosa==0.9.2', 'mega.py', 'gdown', 'onnxruntime', 'pyngrok==4.1.12',
                     'gTTS', 'elevenlabs']
-
-    print("Updating and installing system packages...")
-    for package in packages:
-        print(f"Installing {package}...")
-        subprocess.check_call(['apt-get', 'install', '-qq', '-y', package])
 
     print("Updating and installing pip packages...")
     subprocess.check_call(['pip', 'install', '--upgrade'] + pip_packages)
 
     print('Packages up to date.')
 
-
 def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
-    # Mounting Google Drive
-    if not ForceTemporaryStorage:
-        from google.colab import drive
-
-        if not os.path.exists('/content/drive'):
-            drive.mount('/content/drive')
-        else:
-            print('Drive is already mounted. Proceeding...')
-
     # Function to install dependencies with progress
     def install_packages():
-        packages = ['build-essential', 'python3-dev', 'ffmpeg', 'aria2']
         pip_packages = ['pip', 'setuptools', 'wheel', 'httpx==0.23.0', 'faiss-gpu', 'fairseq', 'gradio==3.34.0',
                         'ffmpeg', 'ffmpeg-python', 'praat-parselmouth', 'pyworld', 'numpy==1.23.5',
                         'numba==0.56.4', 'librosa==0.9.2', 'mega.py', 'gdown', 'onnxruntime', 'pyngrok==4.1.12',
                         'gTTS', 'elevenlabs']
-
-        print("Updating and installing system packages...")
-        for package in packages:
-            print(f"Installing {package}...")
-            subprocess.check_call(['apt-get', 'install', '-qq', '-y', package])
 
         print("Updating and installing pip packages...")
         subprocess.check_call(['pip', 'install', '--upgrade'] + pip_packages)
@@ -95,11 +74,10 @@ def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
 
     # Check if CachedRVC.tar.gz exists
     if ForceTemporaryStorage:
-        file_path = '/content/CachedRVC.tar.gz'
+        file_path = '/kaggle/working/CachedRVC.tar.gz'
     else:
-        file_path = '/content/drive/MyDrive/RVC_Cached/CachedRVC.tar.gz'
+        file_path = '/kaggle/working/CachedRVC.tar.gz'
 
-    content_file_path = '/content/CachedRVC.tar.gz'
     extract_path = '/'
 
     if not os.path.exists(file_path):
@@ -123,17 +101,17 @@ def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
         if ForceTemporaryStorage:
             print('Finished downloading CachedRVC.tar.gz.')
         else:
-            print('CachedRVC.tar.gz found on Google Drive. Proceeding to copy and extract...')
+            print('CachedRVC.tar.gz found. Proceeding to extract...')
 
         # Check if ForceTemporaryStorage is True and skip copying if it is
         if ForceTemporaryStorage:
             pass
         else:
-            shutil.copy(file_path, content_file_path)
+            shutil.copy(file_path, file_path)
 
         print('Beginning backup copy operation...')
 
-        with tarfile.open(content_file_path, 'r:gz') as tar:
+        with tarfile.open(file_path, 'r:gz') as tar:
             for member in tar.getmembers():
                 target_path = os.path.join(extract_path, member.name)
                 try:
@@ -141,27 +119,23 @@ def setup_environment(ForceUpdateDependencies, ForceTemporaryStorage):
                 except Exception as e:
                     print('Failed to extract a file (this isn\'t normal)... forcing an update to compensate')
                     ForceUpdateDependencies = True
-            print(f'Extraction of {content_file_path} to {extract_path} completed.')
+            print(f'Extraction of {file_path} to {extract_path} completed.')
 
         if ForceUpdateDependencies:
             install_packages()
             ForceUpdateDependencies = False
     else:
         print('CachedRVC.tar.gz not found. Proceeding to create an index of all current files...')
-        scan_and_write('/usr/', '/content/usr_files.csv')
+        scan_and_write('/usr/', '/kaggle/working/usr_files.csv')
 
         install_packages()
 
-        scan_and_write('/usr/', '/content/usr_files_new.csv')
-        changed_files = compare_files('/content/usr_files.csv', '/content/usr_files_new.csv')
+        scan_and_write('/usr/', '/kaggle/working/usr_files_new.csv')
+        changed_files = compare_files('/kaggle/working/usr_files.csv', '/kaggle/working/usr_files_new.csv')
 
-        with tarfile.open('/content/CachedRVC.tar.gz', 'w:gz') as new_tar:
+        with tarfile.open('/kaggle/working/CachedRVC.tar.gz', 'w:gz') as new_tar:
             for file in changed_files:
                 new_tar.add(file)
                 print(f'Added to tar: {file}')
 
-        os.makedirs('/content/drive/MyDrive/RVC_Cached', exist_ok=True)
-        shutil.copy('/content/CachedRVC.tar.gz', '/content/drive/MyDrive/RVC_Cached/CachedRVC.tar.gz')
-        print('Updated CachedRVC.tar.gz copied to Google Drive.')
         print('Dependencies fully up to date; future runs should be faster.')
-
